e93bc150600203b8ffb2aac8d5b338a0abc3444d
Wooseok Oh <ows88@naver.com>
2021-12-21 00:01:13 +0900

    first commit

---
+++ closure.js
@@ -0,0 +1,20 @@
+function factory_movie(title){
+    return {
+        get_title : function (){
+            return title;
+        },
+        set_title : function(_title){
+            title = _title
+        }
+    }
+}
+ghost = factory_movie('Ghost in the shell');
+matrix = factory_movie('Matrix');
+ 
+alert(ghost.get_title());
+alert(matrix.get_title());
+ 
+ghost.set_title('공각기동대');
+ 
+alert(ghost.get_title());
+alert(matrix.get_title());
